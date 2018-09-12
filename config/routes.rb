ffa3cd986872717efbc82252cf1b7811eb643fd0
mackxxx <yathatatha-6666@yahoo.co.jp>
2018-09-12 16:47:25 +0000

    user login2

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,20 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
+  get 'users/index'
+
+  get 'users/show'
+
+  get 'users/new'
+
+  get 'users/create'
+
   root to: 'tasks#index'
 
-  resources :tasks
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
 end
