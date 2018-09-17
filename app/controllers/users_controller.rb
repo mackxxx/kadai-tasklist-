037ddb245fab2756a0aeffcc53dc3208a249a2cc
mackxxx <yathatatha-6666@yahoo.co.jp>
2018-09-17 10:14:37 +0000

    A

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 before_action :require_user_logged_in, only: [:index, :show]  
   def index
     @users = User.all.page(params[:page])
-　end
+ end
 
   def show
     @user = User.find(params[:id])
